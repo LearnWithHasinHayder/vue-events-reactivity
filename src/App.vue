e9ab925d2afaa117f47783eb4963b41f9c51faa9
Hasin Hayder <hasin@leevio.com>
2023-07-20 11:18:16 +0600

    Episode 04 Code

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
 import { ref, reactive } from 'vue'
-const accordionIndex = [
+const accordionIndex = reactive([
   false, false, false, false
-]
+])
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
     <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
 
-    <div class="p-10 bg-gradient-to-br from-pink-50 to-indigo-100 grid place-items-center">
-      <div class="w-6/12 mx-auto rounded border">
+    <div class="w-full p-10 bg-gradient-to-br from-pink-50 to-indigo-100 grid place-items-center">
+      <div class="w-3/4 mx-auto rounded border">
         <div class="bg-white p-10 shadow-sm">
           <h3 class="text-lg font-medium text-gray-800">Several Windows stacked on each other</h3>
           <p class="text-sm font-light text-gray-600 my-3">
