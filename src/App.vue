dc39152a1c5643ea1bcfb7ccf71a5d7e67c905a4
Hasin Hayder <hasin@leevio.com>
2023-07-20 12:23:26 +0600

    Episode 08 Code

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,18 @@ function spacePressed() {
   space.value++
   message.value = "Space Pressed " + space.value + " times"
 }
+
+function pressedWhat(event) {
+  message.value = "You pressed " + event.key
+}
+
+function divClicked(){
+  alert("Div Clicked")
+}
+
+function buttonClicked(){
+  alert("Button Clicked")
+}
 </script>
 
 <template>
@@ -15,14 +27,28 @@ function spacePressed() {
     <h2 class="mt-10">{{ message }}</h2>
     <form action="https://google.com/search">
       <div class="container mx-auto flex space-x-5 justify-center m-5">
-        <input name="q" type="text" id="" class="border border-gray-500 bg-white p-5" @keyup.space="spacePressed()">
+        <input name="q" type="text"  class="border border-gray-500 bg-white p-5" @keyup.space="spacePressed()" placeholder="Press Space">
+      </div>
+      
+      <div class="container mx-auto flex space-x-5 justify-center m-5">
+        <input name="text" type="text" class="border border-gray-500 bg-white p-5" @keyup="pressedWhat($event)" placeholder="Type Something">
       </div>
 
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="message = 'You searched for Horses'">
         Button
       </button>
 
+      <button class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="message = 'Ctrl + Right Click'">
+        Ctrol + Right Click
+      </button>
+
     </form>
+
+    <div class="bg-gray-200 w-[500px] h-80 mt-5 flex items-center justify-center" @click="divClicked()">
+      <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" @click="buttonClicked()" >
+        Button
+      </button>
+    </div>
   </section>
 </template>
 
