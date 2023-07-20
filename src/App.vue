50ed39a126e4b97c3452fd3464bcfc394fbb0d3f
Hasin Hayder <hasin@leevio.com>
2023-07-20 11:14:10 +0600

    Episode 04 Code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { ref } from 'vue'
-const accordionIndex = ref(0)
+import { ref, reactive } from 'vue'
+const accordionIndex = [
+  false, false, false, false
+]
 </script>
 
 <template>
@@ -20,12 +22,12 @@ const accordionIndex = ref(0)
           <!-- What is term -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" @click="accordionIndex = 0">
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" @click="accordionIndex[0]=!accordionIndex[0]">
               <i class="fas fa-plus"></i>
               <h3>What is term?</h3>
             </div>
             <!-- Content -->
-            <div class="px-5 pt-0 text-left pb-5" :class="accordionIndex == 0 ? '' : 'hidden'">
+            <div class="px-5 pt-0 text-left pb-5" :class="accordionIndex[0]? '' : 'hidden'">
               <p class="leading-6 font-light pl-9 ">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi dolor dolorum odio, saepe quibusdam iusto possimus nesciunt dolores assumenda quae totam, doloremque odit. Itaque cum animi, labore debitis deserunt iusto!
               </p>
@@ -35,12 +37,12 @@ const accordionIndex = ref(0)
           <!-- When to use Accordion Components -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" @click="accordionIndex = 1">
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" @click="accordionIndex[1]=!accordionIndex[1]">
               <i class="fas fa-plus"></i>
               <h3>When to use Accordion Components?</h3>
             </div>
             <!-- Content -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex == 1 ? '' : 'hidden'">
+            <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex[1] ? '' : 'hidden'">
               <p class="leading-6 font-light pl-9 ">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut suscipit itaque, sequi incidunt ab sit optio debitis dignissimos doloribus ratione nobis id mollitia maiores eveniet necessitatibus dolorum praesentium! Corrupti, at.
               </p>
@@ -50,12 +52,12 @@ const accordionIndex = ref(0)
           <!-- Accordion Wrapper -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" @click="accordionIndex = 2">
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" @click="accordionIndex[2]=!accordionIndex[2]">
               <i class="fas fa-plus"></i>
               <h3>How can it be defined?</h3>
             </div>
             <!-- Content -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex == 2 ? '' : 'hidden'">
+            <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex[2] ? '' : 'hidden'">
               <p class="leading-6 font-light pl-9">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima facilis labore voluptates provident nam, delectus suscipit nihil optio voluptate quae porro fugiat magni excepturi doloribus.
               </p>
@@ -65,12 +67,12 @@ const accordionIndex = ref(0)
           <!-- Accordion Wrapper -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" @click="accordionIndex = 3">
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" @click="accordionIndex[3]=!accordionIndex[3]">
               <i class="fas fa-plus"></i>
               <h3>Chamber reached do he nothing be?</h3>
             </div>
             <!-- Content -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex == 3 ? '' : 'hidden'">
+            <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex[3] ? '' : 'hidden'">
               <p class="leading-6 font-light pl-9">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam minima maiores magni dolore qui architecto pariatur incidunt repellat quos hic? Eius, eveniet accusantium! Quisquam, quo. Vitae ipsum ad veritatis commodi quasi expedita!
               </p>
