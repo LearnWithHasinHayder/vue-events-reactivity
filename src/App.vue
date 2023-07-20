e39f95b8b1212d9786d14e0c5425526856dbc09b
Hasin Hayder <hasin@leevio.com>
2023-07-20 12:11:34 +0600

    Episode 08 Code

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref } from "vue"
 const space = ref(0)
 const message = ref("Hello Vue 3")
 
-function spacePressed(){
+function spacePressed() {
   space.value++
   message.value = "Space Pressed " + space.value + " times"
 }
@@ -13,9 +13,16 @@ function spacePressed(){
   <section class="container mx-auto flex items-center flex-col">
     <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
     <h2 class="mt-10">{{ message }}</h2>
-    <div class="container mx-auto flex space-x-5 justify-center m-5">
-      <input type="text" name="" id="" class="border border-gray-500 bg-white p-5" @keyup.space="spacePressed()">
-    </div>
+    <form action="https://google.com/search">
+      <div class="container mx-auto flex space-x-5 justify-center m-5">
+        <input name="q" type="text" id="" class="border border-gray-500 bg-white p-5" @keyup.space="spacePressed()">
+      </div>
+
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="message = 'You searched for Horses'">
+        Button
+      </button>
+
+    </form>
   </section>
 </template>
 
