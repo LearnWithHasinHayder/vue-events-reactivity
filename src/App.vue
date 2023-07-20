71deb495455ec2cc64f1394bc229ffe7ba097633
Hasin Hayder <hasin@leevio.com>
2023-07-20 12:07:54 +0600

    Episode 08 Code

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,20 @@
 <script setup>
 import { ref } from "vue"
-const about = ref("Hello")
-const links=[
-  {
-    name: 'Google',
-    url: "https://www.google.com",
-    about:"Google is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware."
-  },
-  {
-    name: 'Microsoft',
-    url: "https://www.microsoft.com",
-    about:"Microsoft Corporation is an American multinational technology company with headquarters in Redmond, Washington. It develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services."
-  },
-  {
-    name: 'Facebook',
-    url: "https://www.facebook.com",
-    about:"Facebook is an American online social media and social networking service based in Menlo Park, California, and a flagship service of the namesake company Facebook, Inc."
-  },
-  {
-    name:"Twitter",
-    url:"https://www.twitter.com",
-    about:"Twitter is an American microblogging and social networking service on which users post and interact with messages known as tweets. Registered users can post, like and retweet tweets, but unregistered users can only read them."
-  }
-]
+const space = ref(0)
+const message = ref("Hello Vue 3")
 
-function setText(text){
-  about.value=text
-  console.log(about.value)
+function spacePressed(){
+  space.value++
+  message.value = "Space Pressed " + space.value + " times"
 }
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
     <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
-    <h2 class="mt-10">{{ about }}</h2>
-    <div class="container mx-auto flex space-x-5 justify-center m-[1000px]">
-      <a
-        v-for="link in links"
-        target="_blank"
-        :key="link.name"
-        href="#"
-        @click.prevent="setText(link.about)"
-        >{{ link.name }}</a
-      >
+    <h2 class="mt-10">{{ message }}</h2>
+    <div class="container mx-auto flex space-x-5 justify-center m-5">
+      <input type="text" name="" id="" class="border border-gray-500 bg-white p-5" @keyup.space="spacePressed()">
     </div>
   </section>
 </template>
