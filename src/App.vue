48e80e6f533274ebf6bec5dd488745ba95d8f8d1
Hasin Hayder <hasin@leevio.com>
2023-07-20 10:40:10 +0600

    Episode 01 Code

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
-const location = "Dhaka"
+import {ref} from 'vue'
+const location = ref("Dhaka")
 
+function changeLocation(newLocation) {
+  location.value = newLocation
+}
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
     <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
     <div class="container mx-auto flex space-x-5 justify-center m-5">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka')">
         Dhaka
       </button>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Rajshahi')">
         Rajshahi
       </button>
     </div>
